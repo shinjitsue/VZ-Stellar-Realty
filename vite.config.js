8d8be26b36c7a4c6bc6fe939d184957ff6e75493
Shinjitsu <71762653+shinjitsue@users.noreply.github.com>
2024-06-17 12:32:27 +0800

    Updated project dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import alias from "@rollup/plugin-alias";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    alias({
+      entries: [
+        {
+          find: "@",
+          replacement: fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      ],
+    }),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+});
